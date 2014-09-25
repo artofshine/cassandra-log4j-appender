138cc28b1dafb42e7ba6866ccf388086afa6e961
Ilya Kuchmin <ikuchmin@osslabs.ru>
2014-09-25 22:11:49 +0400

    Add main class for tests up connection (cherry picked from commit db7e9e5)

---
+++ src/main/java/ru/osslabs/tests/CompatibilityCassandra.java
@@ -0,0 +1,13 @@
+package ru.osslabs.tests;
+
+import com.datastax.logging.appender.CassandraAppender;
+
+/**
+ * Created by ikuchmin on 25.09.14.
+ */
+public class CompatibilityCassandra extends CassandraAppender{
+    public static void main(String[] args) {
+        CompatibilityCassandra appender = new CompatibilityCassandra();
+        appender.initClient();
+    }
+}
